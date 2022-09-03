7607918d855d8314ee4db240479078e00fde17e0
天舟 <t@bytebase.com>
2022-09-03 15:33:14 +0800

    refactor: make modular

--- main.go
+++ main.go
@@ -1,15 +1,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/bytebase/relay/cmd"
+	"github.com/bytebase/relay/hook"
+	"github.com/bytebase/relay/sink"
+	"github.com/flamego/flamego"
+	flag "github.com/spf13/pflag"
+)
+
+const (
+
+	// greetingBanner is the greeting banner.
+	// http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Relay
+	greetingBanner = `
+██████╗ ███████╗██╗      █████╗ ██╗   ██╗
+██╔══██╗██╔════╝██║     ██╔══██╗╚██╗ ██╔╝
+██████╔╝█████╗  ██║     ███████║ ╚████╔╝
+██╔══██╗██╔══╝  ██║     ██╔══██║  ╚██╔╝
+██║  ██║███████╗███████╗██║  ██║   ██║
+╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝
+___________________________________________________________________________________________
+
+`
+	// byeBanner is the bye banner.
+	// http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=BYE
+	byeBanner = `
+██████╗ ██╗   ██╗███████╗
+██╔══██╗╚██╗ ██╔╝██╔════╝
+██████╔╝ ╚████╔╝ █████╗
+██╔══██╗  ╚██╔╝  ██╔══╝
+██████╔╝   ██║   ███████╗
+╚═════╝    ╚═╝   ╚══════╝
+
+`
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
+	flag.Parse()
+
+	f := flamego.Classic()
+	github := hook.NewGitHub()
+	lark := sink.NewLark()
+	hook.Mount(f, "/github", github, []sink.Sinker{lark})
+
+	// Setup signal handlers.
+	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan os.Signal, 1)
+	// Trigger graceful shutdown on SIGINT or SIGTERM.
+	// The default signal sent by the `kill` command is SIGTERM,
+	// which is taken as the graceful shutdown signal for many systems, eg., Kubernetes, Gunicorn.
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		f.Stop()
+		cancel()
+	}()
+
+	fmt.Print(greetingBanner)
+
+	f.Run()
+
+	fmt.Print(byeBanner)
+
+	// Wait for CTRL-C.
+	<-ctx.Done()
 }
--- sink/sink.go
+++ sink/sink.go
@@ -2,74 +2,13 @@ package sink
 
 import (
 	"context"
-	"sync"
-
-	"github.com/flamego/flamego"
-	"github.com/hashicorp/go-multierror"
-	flag "github.com/spf13/pflag"
 )
 
 // Sinker is the interface for receiving the webhook payload from the Hooker
 type Sinker interface {
-	// register registers itself to be used, common tasks include:
-	// 1. Declare flags
-	register(fs *flag.FlagSet)
-	// prepare performs validation checks before to be used, common tasks include:
-	// 1. Check flag values
-	prepare() error
-	// process processes the payload extracted by the Hooker
-	process(c context.Context, path string, payload interface{}) error
-}
-
-var (
-	sinkersMu sync.RWMutex
-	sinkers   = make(map[string][]Sinker)
-)
-
-// Register registers the sinker for path.
-// e.g. If service foo posts the webhook event to path /foo,
-// You can register a sinker at path /foo to receive that webhook event payload.
-func Register(fs *flag.FlagSet, f *flamego.Flame, s Sinker, path string) {
-	if s == nil {
-		panic("sink: Register sinker is nil")
-	}
-
-	sinkersMu.Lock()
-	defer sinkersMu.Unlock()
-	for _, item := range sinkers[path] {
-		if item == s {
-			panic("sink: Register called twice for sinker " + path)
-		}
-	}
-	s.register(fs)
-	sinkers[path] = append(sinkers[path], s)
-}
-
-// Prepare prepares all registered sinks before running (e.g. validate flags).
-func Prepare() error {
-	sinkersMu.Lock()
-	defer sinkersMu.Unlock()
-
-	var result error
-	for _, sinkList := range sinkers {
-		for _, sink := range sinkList {
-			if err := sink.prepare(); err != nil {
-				result = multierror.Append(result, err)
-			}
-		}
-	}
-	return result
-}
-
-// Process iterates over each sink registered for the path and let each sink process the payload.
-func Process(c context.Context, path string, payload interface{}) error {
-	var result error
-	if list := sinkers[path]; list != nil {
-		for _, s := range list {
-			if err := s.process(c, path, payload); err != nil {
-				result = multierror.Append(result, err)
-			}
-		}
-	}
-	return result
+	// Mount is called upon being mount to a hooker, common tasks performed inside Mount:
+	// - Check flag values.
+	Mount() error
+	// Process processes the payload extracted by the Hooker
+	Process(c context.Context, path string, payload interface{}) error
 }
