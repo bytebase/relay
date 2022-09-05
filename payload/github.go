587b79c1399ce89e20becf351d13fc1797b0ef97
tianzhou <t@bytebase.com>
2022-09-05 18:29:22 +0800

    feat: enhance GitHub push event message (#13)

--- payload/github.go
+++ payload/github.go
@@ -1,6 +1,18 @@
 package payload
 
+type GitHubAuthor struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type GitHubCommit struct {
+	Message   string       `json:"message"`
+	Timestamp string       `json:"timestamp"`
+	Author    GitHubAuthor `json:"author"`
+}
+
 type GitHubPushEvent struct {
-	Ref     string `json:"ref"`
-	Compare string `json:"compare"`
+	Ref        string       `json:"ref"`
+	Compare    string       `json:"compare"`
+	HeadCommit GitHubCommit `json:"head_commit"`
 }
